899bf747ed8296ef5c6884b094e1b8da5d6a2cfb
aberdar <sashaberdar@icloud.com>
2022-04-04 22:44:53 +0300

    Update

--- src/personal/Main.java
+++ src/personal/Main.java
@@ -5,10 +5,17 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static boolean inputView;
+    public static String inputLevel;
+    public static double maxDuration;
+    public static int minPrice = 0;
+    public static int maxPrice = 0;
+
+    public static void main(String[] args) throws Exception {
 
         ArrayList<Course> courses = new ArrayList<>();
 
@@ -49,10 +56,50 @@ public class Main {
             System.out.println(ex.getMessage());
         }
 
-        for (Course e : courses) {
-            if (e.getCoursePrice() == 0) {
-                System.out.println(e.getCourseName());
+        userRequest();
+    }
+
+    public static void userRequest() throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Вид курса: \n1.Платный \n2.Бесплатный" );
+        int view = scan.nextInt();
+        inputView = view == 1;
+
+        if (inputView) {
+            System.out.println("Укажите диапазон стоимости: ");
+            System.out.println("От: ");
+            minPrice = scan.nextInt();
+            if (minPrice < 0) {
+                throw new Exception("Минимальная стоимость должна быть больше либо равной нулю.");
             }
+            System.out.println("До: ");
+            maxPrice = scan.nextInt();
+            if (minPrice > maxPrice) {
+                throw new Exception("Минимальная стоимость не может быть больше максимальной.");
+            }
+            if (maxPrice > Math.pow(10, 9)) {
+                throw new Exception("Введено некорректное значение.");
+            }
+        }
+
+        System.out.println("Сложность курса: \n1. Beginner Level" +
+                                            "\n2. Intermediate Level" +
+                                            "\n3. Expert Level" +
+                                            "\n4. All Levels");
+        int levelInput = scan.nextInt();
+        if (levelInput < 1 || levelInput > 4) {
+            throw new Exception("Возможные значения от 1 до 4.");
+        }
+        if (levelInput == 1) inputLevel = "Beginner Level";
+        if (levelInput == 2) inputLevel = "Intermediate Level";
+        if (levelInput == 3) inputLevel = "Expert Level";
+        if (levelInput == 4) inputLevel = "All Levels";
+
+        System.out.println("Максимальная продолжительность курса: ");
+        maxDuration = scan.nextDouble();
+        if (maxDuration <= 0) {
+            throw new Exception("Значение должно быть больше нуля.");
         }
     }
 }
